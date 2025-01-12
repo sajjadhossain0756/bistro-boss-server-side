51843401255a658d6dfe93adac289dde0d76cb70
sajjadhossain <sajjadhossain0756@gmail.com>
2025-01-12 19:34:43 +0600

    make user admin and delete user api done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const PORT = process.env.PORT || 6002
 
 const app = express()
@@ -27,13 +27,72 @@ const client = new MongoClient(uri, {
 
 async function run() {
     const database = client.db("bistroBossResturaunt");
+    const userCollection = database.collection("users");
     const productCollection = database.collection("allProduct");
+    const cartCollection = database.collection("cartItems")
     try {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
+        // get all user from database
+        app.get('/users',async(req,res)=>{
+            const result = await userCollection.find().toArray()
+            res.send(result)
+        })
+        // create user in database
+        app.post('/users',async(req,res)=>{
+            const userData = req.body;
+            const query = {email: userData.email}
+            const existanceUser = await userCollection.findOne(query);
+            if(existanceUser){
+                return res.send({message: 'user already exists', insertedId: null })
+            }
+            const result = await userCollection.insertOne(userData)
+            res.send(result)
+        })
+        // delete user from database
+        app.delete('/users/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
+        })
+        // make admin user in database
+        app.patch('/users/admin/:id',async(req,res)=>{
+            const id = req.params.id
+            const filter = {_id: new ObjectId(id)}
+            const updatedDoc = {
+                $set:{
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter,updatedDoc)
+            res.send(result)
+        })
         app.get('/product',async(req,res)=>{
             
             const result = await productCollection.find().toArray()
+            res.send(result)
+        })
+        
+        // cart item start here
+        // get cart items
+        app.get('/carts', async(req,res)=>{
+            const email = req.query.email;
+            const query = {email: email}
+            const result = await cartCollection.find(query).toArray()
+            res.send(result)
+        })
+        // create cart items
+        app.post('/carts', async(req,res)=>{
+            const cartItem = req.body;
+            const result = await cartCollection.insertOne(cartItem)
+            res.send(result)
+        })
+        // delete items
+        app.delete('/carts/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await cartCollection.deleteOne(query)
             res.send(result)
         })
 
